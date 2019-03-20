1fc41caad38b4f70ae189635a614c7cf21abeed9
Veriny <20imj68@myahs.org>
2019-03-20 23:39:05 +0000

    Sorted project files into src and res

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ extensions = ['DJ', 'Ratto', 'YNTK', 'Reactions', 'Canvas']
 
 @client.event
 async def on_ready():
-    print('Bot online')
+    print('Reaction-Weeb II is online')
 
 if __name__ = = '__main__':
     client.run(TOKEN)
