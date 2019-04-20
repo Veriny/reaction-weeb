49734ef51d8449ffb9d49f19bc71323b03d24ee9
Veriny <20imj68@myahs.org>
2019-04-20 03:39:44 +0000

    Updated reactions

--- src/reactions.py
+++ src/reactions.py
@@ -48,6 +48,28 @@ class reactions(commands.Cog):
         embed = discord.Embed(colour= discord.Color(random.randint(0x000000, 0xFFFFFF)))
         embed.set_image(url="https://i.redd.it/zjhaurjckcq21.png")
         await ctx.send(embed=embed)
+        #https://cdn.discordapp.com/attachments/400909034766467072/568991691672125440/image0.jpg
+    @commands.command()
+    async def gun(self, ctx, member: discord.Member = None):
+        string = ""
+        if member is None:
+            string = ""
+        else:
+            string = "<@!{}>'s post has been deemed cursed and unworthy".format(member.id)
+
+        embed = discord.Embed(description = string, colour= discord.Color(random.randint(0x000000, 0xFFFFFF)))
+        embed.set_image(url="https://cdn.discordapp.com/attachments/400909034766467072/568991691672125440/image0.jpg")
+        await ctx.send(embed=embed)
+    @commands.command()
+    async def uwu(self, ctx, member: discord.Member = None):
+        string = ""
+        if member is None:
+            embed = discord.Embed(description = "The furry <@!{}> has illegally uwu'd and will be euthanized immediately.".format(ctx.message.author.id),  colour= discord.Color(random.randint(0x000000, 0xFFFFFF)))
+            embed.set_image(url="https://i.redd.it/erp4cf8kact21.png")
+        else:
+            embed = discord.Embed(description = "The furry <@!{}> has illegally uwu'd or owo'd and will be euthanized immediately.".format(member.id),  colour= discord.Color(random.randint(0x000000, 0xFFFFFF)))
+            embed.set_image(url="https://i.redd.it/erp4cf8kact21.png")
+        await ctx.send(embed=embed)
     @commands.command()
     async def invite(self, ctx):
         await ctx.send("https://discordapp.com/oauth2/authorize?client_id=558016541514399744&scope=bot&permissions=8")
